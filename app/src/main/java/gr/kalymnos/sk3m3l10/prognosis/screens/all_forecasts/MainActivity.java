7343b577ea81d9b7eb168b0dc47659f6ee18611e
Tomas Michael <tomasmichael995@gmail.com>
2018-02-21 13:19:07 +0100

    Moved MainActivity.java into new package 'all_forecasts'

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
@@ -0,0 +1,23 @@
+package gr.kalymnos.sk3m3l10.prognosis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.CityWeather;
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.LocationWeather;
+import gr.kalymnos.sk3m3l10.prognosis.common.weather_units.OpenWeatherMapUnits;
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
