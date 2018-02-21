8710871c5a35a70faca50d272672cdc89aeb32f6
Tomas Michael <tomasmichael995@gmail.com>
2018-02-21 17:46:15 +0100

    created getImage() function for WeatherImageProvider

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/WeatherImageProvider.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/WeatherImageProvider.java
@@ -1,10 +1,15 @@
 package gr.kalymnos.sk3m3l10.prognosis.util;
 
+import gr.kalymnos.sk3m3l10.prognosis.R;
+
 /**
  * Provides an image for specific weather
  */
 
 public class WeatherImageProvider {
+
+    public static final int DAY_LOWER_BOUND = 7;
+    public static final int DAY_UPPER_BOUND = 19;
 
     // OpenWeatherMap.org weather values
     public static final String CLEAR_SKY = "clear sky";
@@ -28,9 +33,48 @@ public class WeatherImageProvider {
         return instance;
     }
     
-    public int getImage(String weather){
+    public int getImage(String weather, long timeMilli){
+        DateFormater dateFormater = new DateFormater(timeMilli);
+        boolean isDay = dateFormater.isDay(DAY_LOWER_BOUND,DAY_UPPER_BOUND);
+
         switch (weather){
-            
+            case CLEAR_SKY:
+                if (isDay){
+                    return R.drawable.sun;
+                }else{
+                    return R.drawable.moon;
+                }
+                
+            case FEW_CLOUDS:
+                if (isDay){
+                    return R.drawable.sun_few_clouds;
+                }else{
+                    return R.drawable.moon_few_clouds;
+                }
+
+            case SCATTERED_CLOUDS:
+                return R.drawable.clouds;
+                
+            case BROKEN_CLOUDS:
+                return R.drawable.clouds;
+                
+            case RAIN:
+                return R.drawable.scattered_rain;
+                
+            case SHOWER_RAIN:
+                return R.drawable.heavy_rain;
+                
+            case THUNDERSTORM:
+                return R.drawable.storm;
+                
+            case SNOW:
+                return R.drawable.snow;
+                
+            case MIST:
+                return R.drawable.clouds;
+                
+            default:
+                throw new IllegalArgumentException("Unknown weather value from OpenWeatherMap.com");
         }
     }
 }
