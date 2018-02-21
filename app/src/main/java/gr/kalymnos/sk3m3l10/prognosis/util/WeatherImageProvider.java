d04380dff9e1fc879824ea58cb456cde63edf6ca
Tomas Michael <tomasmichael995@gmail.com>
2018-02-21 17:16:16 +0100

    Replaced the enum with Constants in order to use them in a switch statement

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/WeatherImageProvider.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/WeatherImageProvider.java
@@ -6,19 +6,31 @@ package gr.kalymnos.sk3m3l10.prognosis.util;
 
 public class WeatherImageProvider {
 
-    private enum OpenWeatherMapWeatherValues{
+    // OpenWeatherMap.org weather values
+    public static final String CLEAR_SKY = "clear sky";
+    public static final String FEW_CLOUDS = "few clouds";
+    public static final String SCATTERED_CLOUDS = "scattered clouds";
+    public static final String BROKEN_CLOUDS = "broken clouds";
+    public static final String SHOWER_RAIN = "shower rain";
+    public static final String RAIN = "rain";
+    public static final String THUNDERSTORM = "thunderstorm";
+    public static final String SNOW = "snow";
+    public static final String MIST = "mist";
+    
+    private static WeatherImageProvider instance = null;
+    
+    private WeatherImageProvider(){ /* Singleton */}
 
-        CLEAR_SKY("clear sky");
-
-        OpenWeatherMapWeatherValues(String mainWeather){
-            this.mainWeather=mainWeather;
+    public static WeatherImageProvider getInstance(){
+        if (instance==null){
+            instance=new WeatherImageProvider();
         }
-
-        private String mainWeather;
-
-        @Override
-        public String toString() {
-            return this.mainWeather;
+        return instance;
+    }
+    
+    public int getImage(String weather){
+        switch (weather){
+            
         }
     }
 }
