17ce07dbbc9b4d1dd4695be402f3392e7610c2bb
Tomas Michael <tomasmichael995@gmail.com>
2018-02-21 13:24:20 +0100

    AndroidManifest and MainActivity.java were modified because of the movement of the latter to another package

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
@@ -1,4 +1,4 @@
-package gr.kalymnos.sk3m3l10.prognosis;
+package gr.kalymnos.sk3m3l10.prognosis.screens.all_forecasts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import gr.kalymnos.sk3m3l10.prognosis.R;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather.CityWeather;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather.LocationWeather;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather_units.OpenWeatherMapUnits;
