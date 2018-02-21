b5a7ba82d139fa930655fc7549f53a5115ccf26b
Tomas Michael <tomasmichael995@gmail.com>
2018-02-21 10:17:57 +0100

    added Weather obj quered by  a city name (CityWeather) and one by location (LocationWeather)

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/common/CityWeather.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/common/CityWeather.java
@@ -1,34 +1,24 @@
 package gr.kalymnos.sk3m3l10.prognosis.common;
 
-/**
- * Created by skemelio on 20.02.18.
- */
+/*
+* A Weather obj acquired by a city name
+*
+* */
 
 public class CityWeather extends Weather {
 
     private String cityName,countryCode;
 
-    public CityWeather(String cityName,long timeMilli, String mainWeather,
-                       String description, int tempHigh, int tempLow, int humidity, int pressure,
-                       double wind) {
-        super(timeMilli, mainWeather, description, tempHigh, tempLow, humidity, pressure, wind);
-        this.cityName = cityName;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
+    public CityWeather(String cityName, String countryCode,long timeMilli, String mainWeather,
+                       String description, int tempHigh, int tempLow, int humidity,
+                       int pressure, double wind, WeatherUnit weatherUnit) {
+        super(timeMilli, mainWeather, description, tempHigh, tempLow, humidity, pressure, wind, weatherUnit);
+        this.cityName=cityName;
+        this.countryCode=countryCode;
     }
 
     @Override
-    public String toString() {
+    public String getQueryTitle() {
         return cityName+", "+countryCode;
     }
 }
--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/common/LocationWeather.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/common/LocationWeather.java
@@ -1,29 +1,24 @@
 package gr.kalymnos.sk3m3l10.prognosis.common;
 
-/**
- * Created by skemelio on 20.02.18.
- */
+/*
+* A Weather obj acquired by Location (lat,lon)
+*
+* */
 
 public class LocationWeather extends Weather {
 
     private long lat,lon;
 
-    public LocationWeather(long lat,long lon,long timeMilli, String mainWeather, String description, int tempHigh, int tempLow, int humidity, int pressure, double wind) {
-        super(timeMilli, mainWeather, description, tempHigh, tempLow, humidity, pressure, wind);
+    public LocationWeather(long lat,long lon,long timeMilli, String mainWeather, String description,
+                           int tempHigh, int tempLow, int humidity, int pressure, double wind,
+                           WeatherUnit weatherUnit) {
+        super(timeMilli, mainWeather, description, tempHigh, tempLow, humidity, pressure, wind, weatherUnit);
         this.lat=lat;
         this.lon=lon;
     }
 
-    public long getLat() {
-        return lat;
-    }
-
-    public long getLon() {
-        return lon;
-    }
-
     @Override
-    public String toString() {
+    public String getQueryTitle() {
         return String.format("Lat:%d\nLon:%d",this.lat,this.lon);
     }
 }
