33184cb99752c313b88448c5f429807e20ae9d45
Tomas Michael <tomasmichael995@gmail.com>
2018-02-21 15:39:25 +0100

    temporary modification to run a test and see if the adapter works properly

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
@@ -16,9 +16,33 @@ import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Weather> weatherList;
+
+    {
+        weatherList = new ArrayList<>();
+        Weather w1 = new CityWeather("Athens","GR",1519138800,"Clear","Great day, no clouds",15,8,86,55,56.4,new OpenWeatherMapUnits.OpenWeatherMetric());
+        Weather w2 = new CityWeather("Rio De Janeiro","GR",1519138800,"Clear","Great day, no clouds",15,8,86,55,56.4,new OpenWeatherMapUnits.OpenWeatherMetric());
+        Weather w3 = new CityWeather("Kalpaca","GR",1519138800,"Clear","Great day, no clouds",15,8,86,55,56.4,new OpenWeatherMapUnits.OpenWeatherMetric());
+        Weather w4 = new LocationWeather(55555,666666,1519138800,"Clear","Great day, no clouds",15,8,86,55,56.4,new OpenWeatherMapUnits.OpenWeatherMetric());
+        Weather w5 = new CityWeather("Kalymnos","GR",1519138800,"Clear","Great day, no clouds",15,8,86,55,56.4,new OpenWeatherMapUnits.OpenWeatherMetric());
+        weatherList.add(w1);
+        weatherList.add(w2);
+        weatherList.add(w3);
+        weatherList.add(w4);
+        weatherList.add(w5);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rc = this.findViewById(R.id.recyclerview_forecast);
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rc.setLayoutManager(layoutManager);
+
+        WeatherListAdapter adapter = new WeatherListAdapter(this,weatherList);
+        rc.setAdapter(adapter);
     }
 }
