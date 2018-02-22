45437e279ef3f18622cff35e594bb87afb81f30d
Tomas Michael <tomasmichael995@gmail.com>
2018-02-22 17:10:49 +0100

    onWeatherItemClick implementation. If called then it opens detailed activity. The intent holds extras.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/MainActivity.java
@@ -1,5 +1,6 @@
 package gr.kalymnos.sk3m3l10.prognosis.screens.all_forecasts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,6 +16,7 @@ import gr.kalymnos.sk3m3l10.prognosis.common.weather.CityWeather;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather.LocationWeather;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather_units.OpenWeatherMapUnits;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
+import gr.kalymnos.sk3m3l10.prognosis.screens.detail.DetailActivity;
 import gr.kalymnos.sk3m3l10.prognosis.view_mvc.WeatherViewMvc;
 
 import static gr.kalymnos.sk3m3l10.prognosis.view_mvc.WeatherViewMvc.WeatherItemListener;
@@ -46,14 +48,21 @@ public class MainActivity extends AppCompatActivity implements WeatherItemListen
 
     @Override
     public void onWeatherItemClicked(int itemPosition) {
-//        Toast.makeText(this, "Position "+itemPosition, Toast.LENGTH_SHORT).show();
-        List<Weather> list = new ArrayList<>();
-        for (int i=0; i<100; i++){
-            list.add(new CityWeather("Rio de Janeiro","Bra",1519138800,
-                    "clear sky","Great day, no clouds",
-                    15,8,86,55,
-                    56.4,new OpenWeatherMapUnits.OpenWeatherMetric()));
-        }
-        view.bindWeatherItems(list);
+        Weather w = this.weatherList.get(itemPosition);
+
+        Bundle extras = new Bundle();
+        extras.putInt(DetailActivity.IMAGE_KEY,w.getImage());
+        extras.putString(DetailActivity.DATE_KEY,w.getDate());
+        extras.putString(DetailActivity.DESCRIPTION_KEY,w.getDescription());
+        extras.putString(DetailActivity.HIGH_TEMP_KEY,w.getTempHighWithSymbol());
+        extras.putString(DetailActivity.LOW_TEMP_KEY,w.getTempLowWithSymbol());
+        extras.putString(DetailActivity.HUMIDITY_KEY,w.getHumidity());
+        extras.putString(DetailActivity.PRESSURE_KEY,w.getPressure());
+        extras.putString(DetailActivity.WIND_KEY,w.getWind());
+
+        Intent intent = new Intent(this,DetailActivity.class);
+        intent.putExtras(extras);
+
+        this.startActivity(intent);
     }
 }
