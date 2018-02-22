935c2419bed9f0d0ca696ac6da7a3842f1c94154
Tomas Michael <tomasmichael995@gmail.com>
2018-02-22 17:00:19 +0100

    Values are binded directly in onCreate()

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/detail/DetailActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/detail/DetailActivity.java
@@ -1,5 +1,6 @@
 package gr.kalymnos.sk3m3l10.prognosis.screens.detail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,39 +11,38 @@ import gr.kalymnos.sk3m3l10.prognosis.view_mvc.DetailViewMvc;
 
 public class DetailActivity extends AppCompatActivity {
 
-    public static final java.lang.String IMG_KEY = "img key";
-    public static final java.lang.String QUERY_TITLE_KEY = "query title key";
-    public static final java.lang.String WEATHER_KEY = "weather key";
-    public static final java.lang.String DESCRIPTION_KEY = "description key";
-    public static final java.lang.String HIGH_TEMP_KEY = "high temp key";
-    public static final java.lang.String LOW_TEMP_KEY = "low temp key";
-    public static final java.lang.String HUMIDITY_KEY = "humidity key";
-    public static final java.lang.String PRESSURE_KEY = "pressure key";
-    public static final java.lang.String WIND_KEY = "wind key";
+    public static final String DATE_KEY = "date key";
+    public static final String DESCRIPTION_KEY = "description key";
+    public static final String HIGH_TEMP_KEY = "high temp key";
+    public static final String LOW_TEMP_KEY = "low temp key";
+    public static final String HUMIDITY_KEY = "humidity key";
+    public static final String PRESSURE_KEY = "pressure key";
+    public static final String WIND_KEY = "wind key";
+    public static final String IMAGE_KEY= "image key";
     
     private DetailViewMvc detailViewMvc;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         if (savedInstanceState!=null){
-            this.detailViewMvc=new DetailViewMvcImpl(LayoutInflater.from(this),null);
-            
+            Bundle extras = this.getIntent().getExtras();
+            if (extras!=null){
+                this.detailViewMvc=new DetailViewMvcImpl(LayoutInflater.from(this),null);
+
+                this.detailViewMvc.bindDate(extras.getString(DATE_KEY));
+                this.detailViewMvc.bindImage(extras.getInt(IMAGE_KEY));
+                this.detailViewMvc.bindDescription(extras.getString(DESCRIPTION_KEY));
+                this.detailViewMvc.bindHighTemp(extras.getString(HIGH_TEMP_KEY));
+                this.detailViewMvc.bindLowTemp(extras.getString(LOW_TEMP_KEY));
+                this.detailViewMvc.bindHumidity(extras.getString(HUMIDITY_KEY));
+                this.detailViewMvc.bindPressure(extras.getString(PRESSURE_KEY));
+                this.detailViewMvc.bindWind(extras.getString(WIND_KEY));
+            }
             setContentView(R.layout.activity_detail);
         }else{
             // TODO: Do something if the intent does not contain bundled info
         }
-    }
-    
-    private Weather getWeatherFromExtras(Bundle extras){
-        int imgRes = extras.getInt(IMG_KEY);
-        String queryTitle = extras.getString(QUERY_TITLE_KEY);
-        String weather = extras.getString(WEATHER_KEY);
-        String description = extras.getString(DESCRIPTION_KEY);
-        String highTemp = extras.getString(HIGH_TEMP_KEY);
-        String lowTemp = extras.getString(LOW_TEMP_KEY);
-        String humidity = extras.getString(HUMIDITY_KEY);
-        String pressure = extras.getString(PRESSURE_KEY);
-        String wind = extras.getString(WIND_KEY);
     }
 }
