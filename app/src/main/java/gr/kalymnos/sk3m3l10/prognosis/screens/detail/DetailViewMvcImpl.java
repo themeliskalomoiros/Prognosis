b2c45b09b89b8c6bfb3306d4dec55b2f1c46de9d
Tomas Michael <tomasmichael995@gmail.com>
2018-02-22 15:04:51 +0100

    Created detail view implementation.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/detail/DetailViewMvcImpl.java
@@ -0,0 +1,30 @@
+package gr.kalymnos.sk3m3l10.prognosis.screens.detail;
+
+import android.os.Bundle;
+import android.view.View;
+
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
+import gr.kalymnos.sk3m3l10.prognosis.view_mvc.DetailViewMvc;
+
+/**
+ * Created by skemelio on 22.02.18.
+ */
+
+public class DetailViewMvcImpl implements DetailViewMvc {
+
+    
+    @Override
+    public View getRootView() {
+        return null;
+    }
+
+    @Override
+    public Bundle getViewState() {
+        return null;
+    }
+
+    @Override
+    public void bindWeather(Weather weather) {
+
+    }
+}
