7ba6cdfbc574e280b477fef76110f8ad880a4665
Tomas Michael <tomasmichael995@gmail.com>
2018-02-22 14:13:26 +0100

    DetailActivity creation with parent the MainActivity.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/detail/DetailActivity.java
@@ -0,0 +1,14 @@
+package gr.kalymnos.sk3m3l10.prognosis.screens.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import gr.kalymnos.sk3m3l10.prognosis.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
