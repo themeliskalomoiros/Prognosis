23a2fbeef4c230bbdccb2fbb8373a066bdf86d97
Tomas Michael <tomasmichael995@gmail.com>
2018-02-22 11:02:12 +0100

    Added constructor for the MVC View

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/WeatherViewMvcImpl.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/WeatherViewMvcImpl.java
@@ -1,6 +1,7 @@
 package gr.kalymnos.sk3m3l10.prognosis.screens.all_forecasts;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 
 import java.util.List;
 
+import gr.kalymnos.sk3m3l10.prognosis.R;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
 import gr.kalymnos.sk3m3l10.prognosis.view_mvc.WeatherViewMvc;
 
@@ -20,8 +22,30 @@ public class WeatherViewMvcImpl implements WeatherViewMvc {
     private RecyclerView recyclerView;
     private WeatherListAdapter adapter;
 
-    public WeatherViewMvcImpl(LayoutInflater inflater, ViewGroup parent) {
-        
+    public WeatherViewMvcImpl(LayoutInflater inflater, ViewGroup parent, WeatherListAdapter adapter) {
+        this.rootView = inflater.inflate(R.layout.activity_main,parent,false);
+        this.adapter = adapter;
+
+        // recycler view initialization
+        this.recyclerView=this.rootView.findViewById(R.id.recyclerview_forecast);
+        /*
+         * A LinearLayoutManager is responsible for measuring and positioning item views within a
+         * RecyclerView into a linear list. This means that it can produce either a horizontal or
+         * vertical list depending on which parameter you pass in to the LinearLayoutManager
+         * constructor. In our case, we want a vertical list, so we pass in the constant from the
+         * LinearLayoutManager class for vertical lists, LinearLayoutManager.VERTICAL.
+         *
+         * There are other LayoutManagers available to display your data in uniform grids,
+         * staggered grids, and more! See the developer documentation for more details.
+         *
+         * The third parameter (shouldReverseLayout) should be true if you want to reverse your
+         * layout. Generally, this is only true with horizontal lists that need to support a
+         * right-to-left layout.
+         */
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(inflater.getContext(), LinearLayoutManager.VERTICAL, false);
+        this.recyclerView.setLayoutManager(layoutManager);
+        this.recyclerView.setAdapter(adapter);
     }
 
     @Override
