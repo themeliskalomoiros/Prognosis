5ab52b787052b662d1ea6fa2a35e1a459da7488d
Tomas Michael <tomasmichael995@gmail.com>
2018-02-23 21:35:10 +0100

    Settings are loaded from the xml resource.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
@@ -1,11 +1,21 @@
 package gr.kalymnos.sk3m3l10.prognosis.screens.settings;
 
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import gr.kalymnos.sk3m3l10.prognosis.R;
 
 /**
  * This fragment will host the app settings
  */
 
 public class SettingsFragment extends PreferenceFragment {
-    
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from the XML resourse
+        this.addPreferencesFromResource(R.xml.preferences);
+    }
 }
