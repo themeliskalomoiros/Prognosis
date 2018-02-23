d310f706362d396685a14a0ddffc33721af5db94
Tomas Michael <tomasmichael995@gmail.com>
2018-02-23 21:32:45 +0100

    Created settings fragment.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
@@ -0,0 +1,11 @@
+package gr.kalymnos.sk3m3l10.prognosis.screens.settings;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * This fragment will host the app settings
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+    
+}
