75017b08c669a5404d26845fb98fc05c3c45e451
Tomas Michael <tomasmichael995@gmail.com>
2018-02-23 21:39:32 +0100

    Created SettingsActivity which will host the settigns fragment.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsActivity.java
@@ -0,0 +1,15 @@
+package gr.kalymnos.sk3m3l10.prognosis.screens.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gr.kalymnos.sk3m3l10.prognosis.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
