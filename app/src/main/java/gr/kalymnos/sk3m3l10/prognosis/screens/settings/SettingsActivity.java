cc2ec7cc43e6ef7584d1a72f7b17b84ef51474b3
Tomas Michael <tomasmichael995@gmail.com>
2018-02-23 22:03:55 +0100

    Added home button to SettingsActivity and implement its functionality.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsActivity.java
@@ -2,6 +2,7 @@ package gr.kalymnos.sk3m3l10.prognosis.screens.settings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import gr.kalymnos.sk3m3l10.prognosis.R;
 
@@ -11,5 +12,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                // if the home button is clicked, just go back to the previous activity
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
