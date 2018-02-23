ce1cee72af26898454550b0eaf6ee92990a9fc5b
Tomas Michael <tomasmichael995@gmail.com>
2018-02-23 10:57:43 +0100

    Created a Weather Service object.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/WeatherService.java
@@ -0,0 +1,14 @@
+package gr.kalymnos.sk3m3l10.prognosis.model_mvc;
+
+import java.util.List;
+
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
+
+/**
+ * A weather service which supply us with live Weather data.
+ */
+
+public interface WeatherService {
+    public List<Weather> getWeatherForecast(String cityName);
+    public List<Weather> getWeatherForecast(double lat, double lon);
+}
