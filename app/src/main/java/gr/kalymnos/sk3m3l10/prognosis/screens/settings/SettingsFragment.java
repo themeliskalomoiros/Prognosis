1b1da624b3a9edcffdde3425c2c1d6d405e24ad3
Tomas Michael <tomasmichael995@gmail.com>
2018-02-24 12:54:44 +0100

    Moved addition of settings layout logic to onCreatePreferences().

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
@@ -12,17 +12,11 @@ import gr.kalymnos.sk3m3l10.prognosis.R;
  */
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Load the preferences from the XML resourse
-        this.addPreferencesFromResource(R.xml.preferences);
-    }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-
+        // Load the preferences from the XML resourse
+        this.addPreferencesFromResource(R.xml.preferences);
     }
 
     @Override
