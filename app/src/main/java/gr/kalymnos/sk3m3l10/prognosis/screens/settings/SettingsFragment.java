299a5da5a3edd520d56cf83de851648c221cd554
Tomas Michael <tomasmichael995@gmail.com>
2018-02-24 13:15:26 +0100

    Set location setting summary when user open settings.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
@@ -3,7 +3,9 @@ package gr.kalymnos.sk3m3l10.prognosis.screens.settings;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 
 import gr.kalymnos.sk3m3l10.prognosis.R;
 
@@ -13,10 +15,20 @@ import gr.kalymnos.sk3m3l10.prognosis.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
 
+    private static final int QUERY_LOCATION_INDEX = 0;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from the XML resourse
         this.addPreferencesFromResource(R.xml.preferences);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        String storedLocation = sharedPreferences.getString(getActivity().getString(R.string.pref_location_key)
+                ,getActivity().getString(R.string.pref_location_default));
+
+        PreferenceScreen settings = getPreferenceScreen();
+        Preference locationSetting = settings.getPreference(QUERY_LOCATION_INDEX);
+        locationSetting.setSummary(storedLocation);
     }
 
     @Override
