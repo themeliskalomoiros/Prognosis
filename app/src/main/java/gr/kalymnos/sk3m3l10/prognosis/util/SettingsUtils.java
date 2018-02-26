190439f66843669176c4e6a7f7ed71c039bdcd4d
Tomas Michael <tomasmichael995@gmail.com>
2018-02-26 16:48:54 +0100

    Default values for preferences are also gained from SettingUtils.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/SettingsUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/SettingsUtils.java
@@ -34,16 +34,32 @@ public class SettingsUtils {
         return this.context.getString(R.string.pref_location_key);
     }
 
+    public String getCityDefaultValue(){
+        return this.context.getString(R.string.pref_location_default);
+    }
+
     public String getDeviceLocationPrefKey(){
         return this.context.getString(R.string.pref_enable_gps_search_key);
+    }
+
+    public boolean getDeviceLocationDefaultValue(){
+        return this.context.getResources().getBoolean(R.bool.gps_search_by_default);
     }
 
     public String getNotificationEnabledPrefKey(){
         return this.context.getString(R.string.pref_notifications_enabled_key);
     }
 
+    public boolean getNotificationEnabledDefaultValue(){
+        return this.context.getResources().getBoolean(R.bool.weather_notifications_by_default);
+    }
+
     public String getNotificationTimePrefKey(){
         return this.context.getString(R.string.pref_notification_time_key);
+    }
+
+    public String getNotificationTimeDefaultEntry(){
+        return this.context.getString(R.string.pref_notification_time_entry_three);
     }
 
 }
