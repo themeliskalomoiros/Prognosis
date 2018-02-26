659e2c888edcc590a57765b02f3e1c464beeb51c
Tomas Michael <tomasmichael995@gmail.com>
2018-02-26 16:19:01 +0100

    Created 2 new methods to avoid duplication.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/settings/SettingsFragment.java
@@ -28,31 +28,12 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
         this.initialize();
 
-        // set city preference summary
-        String cityName = this.getPreferenceScreen().getSharedPreferences()
-                .getString(cityNameKey,cityNameDefaultValue);
-        cityPref.setSummary(cityName);
+        this.setLocationSummary();
 
         // set notification time preference summary
-        notificationTimePref.setSummary(notificationTimePref.getEntry());
+        this.notificationTimePref.setSummary(notificationTimePref.getEntry());
 
-        // if notifications are not checked then disable the time setting
-        if (this.notificationsEnabledPref.isChecked()){
-            this.notificationTimePref.setEnabled(true);
-        }else{
-            this.notificationTimePref.setEnabled(false);
-        }
-    }
-
-    private void initialize(){
-        this.cityNameKey = this.getString(R.string.pref_location_key);
-        this.cityNameDefaultValue = this.getString(R.string.pref_location_default);
-        this.notificationTimeKey = this.getString(R.string.pref_notification_time_key);
-        this.notificationsEnabledKey = this.getString(R.string.pref_notifications_enabled_key);
-
-        this.cityPref = this.findPreference(cityNameKey);
-        this.notificationTimePref = (ListPreference) this.findPreference(notificationTimeKey);
-        this.notificationsEnabledPref = (CheckBoxPreference) this.findPreference(notificationsEnabledKey);
+        this.setNotificationsTimeEnabled();
     }
 
     @Override
@@ -70,17 +51,36 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(this.cityNameKey)){
-            String cityName = this.getPreferenceScreen().getSharedPreferences()
-                    .getString(cityNameKey,cityNameDefaultValue);
-            cityPref.setSummary(cityName);
+            this.setLocationSummary();
         }else if(key.equals(this.notificationTimeKey)){
             notificationTimePref.setSummary(notificationTimePref.getEntry());
         }else if (key.equals(this.notificationsEnabledKey)){
-            if (this.notificationsEnabledPref.isChecked()){
-                this.notificationTimePref.setEnabled(true);
-            }else{
-                this.notificationTimePref.setEnabled(false);
-            }
+            this.setNotificationsTimeEnabled();
         }
+    }
+
+    private void setNotificationsTimeEnabled(){
+        if (this.notificationsEnabledPref.isChecked()){
+            this.notificationTimePref.setEnabled(true);
+        }else{
+            this.notificationTimePref.setEnabled(false);
+        }
+    }
+
+    private void setLocationSummary(){
+        String cityName = this.getPreferenceScreen().getSharedPreferences()
+                .getString(cityNameKey,cityNameDefaultValue);
+        cityPref.setSummary(cityName);
+    }
+
+    private void initialize(){
+        this.cityNameKey = this.getString(R.string.pref_location_key);
+        this.cityNameDefaultValue = this.getString(R.string.pref_location_default);
+        this.notificationTimeKey = this.getString(R.string.pref_notification_time_key);
+        this.notificationsEnabledKey = this.getString(R.string.pref_notifications_enabled_key);
+
+        this.cityPref = this.findPreference(cityNameKey);
+        this.notificationTimePref = (ListPreference) this.findPreference(notificationTimeKey);
+        this.notificationsEnabledPref = (CheckBoxPreference) this.findPreference(notificationsEnabledKey);
     }
 }
