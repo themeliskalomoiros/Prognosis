b61826ed15b02cacf72483f91f9c5643a068c9c6
Tomas Michael <tomasmichael995@gmail.com>
2018-02-26 22:54:15 +0100

    Added a thread worker which fires a notification.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/sync/WeatherReminderJobService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/sync/WeatherReminderJobService.java
@@ -2,6 +2,10 @@ package gr.kalymnos.sk3m3l10.prognosis.sync;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import gr.kalymnos.sk3m3l10.prognosis.util.NotificationUtils;
 
 /**
  *  WeatherReminderJobService extends JobService because
@@ -13,9 +17,30 @@ import android.app.job.JobService;
  */
 
 public class WeatherReminderJobService extends JobService {
+
+    private AsyncTask backgroundTask;
+
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        this.backgroundTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Context context = WeatherReminderJobService.this;
+                NotificationUtils.showWeatherNotification(context);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                /* Signal here that the job is finished
+                   (because the job is finised only when AsyncTask is finish).
+                   */
+                jobFinished(params,false);
+            }
+        };
+
+        this.backgroundTask.execute();
+        return true; /* Signals that our service is still doing some work (because of the thread)*/
     }
 
     @Override
