57c8e6b8b0b3081fa8924f1e14329d9a8d0a27a7
Tomas Michael <tomasmichael995@gmail.com>
2018-02-26 23:05:09 +0100

    Replaced classes from android.app.job to synonymous ones from com.firebase.jobdispatcher.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/sync/WeatherReminderJobService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/sync/WeatherReminderJobService.java
@@ -1,9 +1,10 @@
 package gr.kalymnos.sk3m3l10.prognosis.sync;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.content.Context;
 import android.os.AsyncTask;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
 import gr.kalymnos.sk3m3l10.prognosis.util.NotificationUtils;
 
