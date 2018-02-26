266b24ac3a13658058df7bc670f40fb314bc5393
Tomas Michael <tomasmichael995@gmail.com>
2018-02-26 21:51:59 +0100

    Creating showWeatherNotification(). Implement Notification channels for Android O and later.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/NotificationUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/NotificationUtils.java
@@ -1,10 +1,13 @@
 package gr.kalymnos.sk3m3l10.prognosis.util;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 
 import gr.kalymnos.sk3m3l10.prognosis.R;
 import gr.kalymnos.sk3m3l10.prognosis.screens.all_forecasts.MainActivity;
@@ -12,6 +15,20 @@ import gr.kalymnos.sk3m3l10.prognosis.screens.all_forecasts.MainActivity;
 public class NotificationUtils {
 
     private static final int WEATHER_NOTIFICATION_INTENT_ID=1990;
+    private static final String NOTIFICATION_CHANNEL_ID = "weather notification channel id";
+
+    public static void showWeatherNotification(Context context){
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since Android O we have to create a notification channel to display notifications
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    context.getString(R.string.weather_notification_channel_name)
+                    ,NotificationManager.IMPORTANCE_HIGH); /* IMPORTANCE_HIGH so notification must pop up*/
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     /*
        This method will create the pending intent which will trigger
