ef02dee569af0ccab1da28f7d673bdab2796a9f8
Tomas Michael <tomasmichael995@gmail.com>
2018-02-27 16:11:05 +0100

    Removed getWeatherForecast() which takes countryCode as parameter. Added getCurrentWeather().

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/WeatherService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/WeatherService.java
@@ -9,7 +9,10 @@ import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
  */
 
 public interface WeatherService {
+
+    public Weather getCurrentWeather(String cityName);
+    public Weather getCurrentWeather(double lat, double lon);
+
     public List<Weather> getWeatherForecast(String cityName);
-    public List<Weather> getWeatherForecast(String cityName,String countryCode);
     public List<Weather> getWeatherForecast(double lat, double lon);
 }
