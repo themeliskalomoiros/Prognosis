29814afa7b4d04fa1ffb10ac142fcb85c73f94f7
Tomas Michael <tomasmichael995@gmail.com>
2018-02-27 15:04:32 +0100

    Created scheduleWeatherReminder(). For now it fires a job every some seconds. Later it will fire the job depending on user defined time interval.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/ReminderUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/ReminderUtils.java
@@ -1,5 +1,19 @@
 package gr.kalymnos.sk3m3l10.prognosis.util;
 
+import android.content.Context;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
+
+import gr.kalymnos.sk3m3l10.prognosis.sync.WeatherReminderJobService;
+
 /**
  * This class is responsible to schedule jobs.
  */
@@ -8,6 +22,27 @@ public class ReminderUtils {
 
     private static final String REMINDER_JOB_TAG = "weather notification reminder tag";
 
-    // Track if the job was initialized
     private static boolean jobInitialized = false;
+
+    synchronized public static void scheduleWeatherReminder(Context context){
+        if (jobInitialized){
+            return;
+        }
+
+        // Setting up and starting a job with FirebaseJobDispatcher
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+
+        // TODO: Provide constraints for the job. Internet connection and user defined time interval.
+        Job job = dispatcher.newJobBuilder()
+                .setService(WeatherReminderJobService.class)
+                .setTag(REMINDER_JOB_TAG)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(1,3))
+                .setReplaceCurrent(false)
+                .build();
+        dispatcher.mustSchedule(job);
+        jobInitialized=true;
+    }
 }
