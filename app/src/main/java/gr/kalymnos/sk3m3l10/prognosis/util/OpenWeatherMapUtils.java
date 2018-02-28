da117af27dd64537dcab38d8285b7e0f97dfb9c7
Tomas Michael <tomasmichael995@gmail.com>
2018-02-28 19:20:03 +0100

    Created buildUrlWithCityQuery().

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/OpenWeatherMapUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/OpenWeatherMapUtils.java
@@ -1,5 +1,11 @@
 package gr.kalymnos.sk3m3l10.prognosis.util;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Network utilities for OpenWeatherMap.com
  */
@@ -20,4 +26,23 @@ public class OpenWeatherMapUtils implements NetworkUtils {
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
     private static final String CURRENT_WEATHER_URL = BASE_URL+"weather";
     private static final String FORECAST_URL = BASE_URL+"forecast";
+
+    private static final String CLASS_TAG = OpenWeatherMapUtils.class.getSimpleName();
+
+    private URL buildUrlWithCityQuery(String cityQuery) {
+        Uri weatherQueryUri = Uri.parse(CURRENT_WEATHER_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, cityQuery)
+                .appendQueryParameter(FORMAT_PARAM, FORMAT)
+                .appendQueryParameter(UNITS_PARAM, UNITS)
+                .appendQueryParameter(API_KEY_PARAM,API_KEY)
+                .build();
+        try {
+            URL weatherQueryUrl = new URL(weatherQueryUri.toString());
+            Log.d(CLASS_TAG, "URL: " + weatherQueryUrl);
+            return weatherQueryUrl;
+        } catch (MalformedURLException e) {
+            Log.e(CLASS_TAG, e.getMessage());
+            return null;
+        }
+    }
 }
