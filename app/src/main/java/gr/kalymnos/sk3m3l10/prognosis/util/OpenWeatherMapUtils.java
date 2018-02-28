d742d6f698b32a671027239f2ff5077ef825d93a
Tomas Michael <tomasmichael995@gmail.com>
2018-02-28 19:23:16 +0100

    Renamed constants and a method.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/OpenWeatherMapUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/OpenWeatherMapUtils.java
@@ -19,21 +19,22 @@ public class OpenWeatherMapUtils implements NetworkUtils {
     private static final String API_KEY_PARAM = "appid";
 
     // OpenWeatherMap.org parameter values
-    private static final String FORMAT = "json";
-    private static final String UNITS = "metric";
+    private static final String FORMAT_JSON = "json";
+    private static final String UNITS_METRIC = "metric";
     private static final String API_KEY = "b2b88ba02f81b823def0b5ca5dd0471f";
 
+    // Urls
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
     private static final String CURRENT_WEATHER_URL = BASE_URL+"weather";
     private static final String FORECAST_URL = BASE_URL+"forecast";
 
     private static final String CLASS_TAG = OpenWeatherMapUtils.class.getSimpleName();
 
-    private URL buildUrlWithCityQuery(String cityQuery) {
+    private URL buildCurrentWeatherUrlWithCityQuery(String cityQuery) {
         Uri weatherQueryUri = Uri.parse(CURRENT_WEATHER_URL).buildUpon()
                 .appendQueryParameter(QUERY_PARAM, cityQuery)
-                .appendQueryParameter(FORMAT_PARAM, FORMAT)
-                .appendQueryParameter(UNITS_PARAM, UNITS)
+                .appendQueryParameter(FORMAT_PARAM, FORMAT_JSON)
+                .appendQueryParameter(UNITS_PARAM, UNITS_METRIC)
                 .appendQueryParameter(API_KEY_PARAM,API_KEY)
                 .build();
         try {
