ad47820fb5a5e669c0cf69d9f997909040f561ca
Tomas Michael <tomasmichael995@gmail.com>
2018-02-28 21:50:19 +0100

    Created OpenWeatherMapService.java.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherMapService.java
@@ -0,0 +1,33 @@
+package gr.kalymnos.sk3m3l10.prognosis.model_mvc;
+
+import android.location.Location;
+
+import java.util.List;
+
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
+
+/**
+ * The weather service from OpenWeatherMap.com
+ */
+
+public class OpenWeatherMapService implements WeatherService {
+    @Override
+    public Weather getCurrentWeather(String cityName) {
+        return null;
+    }
+
+    @Override
+    public Weather getCurrentWeather(Location location) {
+        return null;
+    }
+
+    @Override
+    public List<Weather> getWeatherForecast(String cityName) {
+        return null;
+    }
+
+    @Override
+    public List<Weather> getWeatherForecast(Location location) {
+        return null;
+    }
+}
