4f54ba8ce4efb5300a711b16c5b611fc396294b9
Tomas Michael <tomasmichael995@gmail.com>
2018-02-28 19:36:59 +0100

    Added Latitude-Longitute parameters.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/OpenWeatherMapUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/OpenWeatherMapUtils.java
@@ -1,5 +1,6 @@
 package gr.kalymnos.sk3m3l10.prognosis.util;
 
+import android.location.Location;
 import android.net.Uri;
 import android.util.Log;
 
@@ -17,11 +18,14 @@ public class OpenWeatherMapUtils implements NetworkUtils {
     private static final String FORMAT_PARAM = "mode";
     private static final String UNITS_PARAM = "units";
     private static final String API_KEY_PARAM = "appid";
+    private static final String LAT_PARAM = "lat";
+    private static final String LON_PARAM = "lon";
 
     // OpenWeatherMap.org parameter values
     private static final String FORMAT_JSON = "json";
     private static final String UNITS_METRIC = "metric";
     private static final String API_KEY = "b2b88ba02f81b823def0b5ca5dd0471f";
+    
 
     // Urls
     private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
