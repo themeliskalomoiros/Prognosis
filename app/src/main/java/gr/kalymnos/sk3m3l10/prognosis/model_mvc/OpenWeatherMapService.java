2ff9b017bff2ad4f3534c09ac1d32840480ea064
Tomas Michael <tomasmichael995@gmail.com>
2018-02-28 22:50:08 +0100

    Adding key constants from OpenWeatherMap api and utility methods of JsonAssembler along the way.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherMapService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherMapService.java
@@ -2,6 +2,7 @@ package gr.kalymnos.sk3m3l10.prognosis.model_mvc;
 
 import android.location.Location;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -44,7 +45,11 @@ public class OpenWeatherMapService implements WeatherService {
 
         // Json keys
         private static final String CITY_NAME = "name";
+        private static final String DATE_TIME = "dt";
         private static final String CITY = "city";
+        private static final String SYS = "sys";
+        private static final String COUNTRY = "country";
+        private static final String LIST = "list";
 
         private static final int TYPE_CURRENT_WEATHER=0;
         private static final int TYPE_FORECAST=1;
@@ -58,21 +63,51 @@ public class OpenWeatherMapService implements WeatherService {
         }
 
         private String getCityName(){
-            String city=null;
-
-
             switch (this.type){
                 case TYPE_CURRENT_WEATHER:
                     return rootObj.optString(CITY_NAME);
                 case TYPE_FORECAST:
-                    JSONObject cityObj = rootObj.optJSONObject(CITY);
-                    return cityObj.optString(CITY_NAME);
+                    JSONObject city = rootObj.optJSONObject(CITY);
+                    return city.optString(CITY_NAME);
                 default:
                     throw new IllegalArgumentException(CLASS_TAG+": Unknown responseType.");
             }
         }
 
+        private String getCountryCode(){
+            switch (this.type){
+                case TYPE_CURRENT_WEATHER:
+                    JSONObject sys = this.rootObj.optJSONObject(SYS);
+                    return sys.optString(COUNTRY);
+                case TYPE_FORECAST:
+                    JSONObject city = rootObj.optJSONObject(CITY);
+                    return city.optString(COUNTRY);
+                default:
+                    throw new IllegalArgumentException(CLASS_TAG+": Unknown responseType.");
+            }
+        }
 
+        private long getTimeMilli(){
+            switch (this.type){
+                case TYPE_CURRENT_WEATHER:
+                    return this.rootObj.optLong(DATE_TIME);
+                default:
+                    throw new IllegalArgumentException(CLASS_TAG+": responseType " +
+                            "must be TYPE_CURRENT_WEATHER for this call.");
+            }
+        }
+
+        // This is used for forecasts where every Weather object has its own time-milli.
+        private long getTimeMilli(int index){
+            switch (this.type){
+                case TYPE_FORECAST:
+                    JSONArray list = this.rootObj.optJSONArray(LIST);
+                    return list.optJSONObject(index).optLong(DATE_TIME);
+                default:
+                    throw new IllegalArgumentException(CLASS_TAG+": responseType " +
+                            "type must be TYPE_FORECAST for this call.");
+            }
+        }
 
     }
 }
