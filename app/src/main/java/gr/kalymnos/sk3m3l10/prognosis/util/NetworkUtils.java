b2aae537402170530cf119e478d48fbe792ed785
Tomas Michael <tomasmichael995@gmail.com>
2018-02-28 17:48:30 +0100

    Added getResponseFromHttpUrl() to fetch an HTTP response.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/NetworkUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/util/NetworkUtils.java
@@ -1,8 +1,42 @@
 package gr.kalymnos.sk3m3l10.prognosis.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * This class will be responsible for handling network operations.
  */
 
-public class NetworkUtils {
+public interface NetworkUtils {
+
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response, null if no response
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            String response = null;
+            if (hasInput) {
+                response = scanner.next();
+            }
+            scanner.close();
+            return response;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
