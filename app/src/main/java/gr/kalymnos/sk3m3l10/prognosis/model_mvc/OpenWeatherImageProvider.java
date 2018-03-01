7bba62045759d697da5beea3de6b252c50fd9e0e
Tomas Michael <tomasmichael995@gmail.com>
2018-03-01 03:11:19 +0100

    Created OpenWeatherImageProvider.java

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherImageProvider.java
@@ -0,0 +1,12 @@
+package gr.kalymnos.sk3m3l10.prognosis.model_mvc;
+
+/**
+ * Get proper drawable for OpenWeatherMap service.
+ */
+
+public class OpenWeatherImageProvider implements WeatherServiceImageProvider {
+    @Override
+    public int getImage(Object... params) {
+        return 0;
+    }
+}
