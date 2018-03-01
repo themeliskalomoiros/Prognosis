7512a0632b17c79aa3c763f53bfbf20455d0d3d9
Tomas Michael <tomasmichael995@gmail.com>
2018-03-01 01:38:17 +0100

    OpenWeatherMapService removed references from LocationWeather. getCurrentWeather methods use assembleWeather() instead of creating Weather obj manualy.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherMapService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherMapService.java
@@ -13,8 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 
-import gr.kalymnos.sk3m3l10.prognosis.common.weather.CityWeather;
-import gr.kalymnos.sk3m3l10.prognosis.common.weather.LocationWeather;
+import gr.kalymnos.sk3m3l10.prognosis.common.weather.OpenWeather;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather.Weather;
 import gr.kalymnos.sk3m3l10.prognosis.common.weather_units.OpenWeatherMapUnits;
 import gr.kalymnos.sk3m3l10.prognosis.util.NetworkUtils;
@@ -36,19 +35,8 @@ public class OpenWeatherMapService implements WeatherService {
             String httpResponse = NetworkUtils.getResponseFromHttpUrl(url);
             JsonAssembler assembler = new JsonAssembler(httpResponse,JsonAssembler.TYPE_CURRENT_WEATHER);
 
-            String city = assembler.getCityName();
-            String country = assembler.getCountryCode();
-            long time = assembler.getTimeMilli();
-            String mainWeather = assembler.getMainWeather();
-            String description = assembler.getDescription();
-            int tempMax = assembler.getMaxTemp();
-            int tempMin = assembler.getMinTemp();
-            int humidity = assembler.getHumidity();
-            int pressure = assembler.getPressure();
-            double windSpeed = assembler.getWind();
+            return this.assembleWeather(assembler);
 
-            return new CityWeather(city,country,time,mainWeather,description,tempMax,tempMin,humidity
-                    ,pressure,windSpeed,new OpenWeatherMapUnits.OpenWeatherMetric());
         } catch (IOException e) {
             Log.e(CLASS_TAG,e.getMessage());
         } catch (JSONException e) {
@@ -66,19 +54,8 @@ public class OpenWeatherMapService implements WeatherService {
             String httpResponse = NetworkUtils.getResponseFromHttpUrl(url);
             JsonAssembler assembler = new JsonAssembler(httpResponse,JsonAssembler.TYPE_CURRENT_WEATHER);
 
-            String city = assembler.getCityName();
-            String country = assembler.getCountryCode();
-            long time = assembler.getTimeMilli();
-            String mainWeather = assembler.getMainWeather();
-            String description = assembler.getDescription();
-            int tempMax = assembler.getMaxTemp();
-            int tempMin = assembler.getMinTemp();
-            int humidity = assembler.getHumidity();
-            int pressure = assembler.getPressure();
-            double windSpeed = assembler.getWind();
+            return this.assembleWeather(assembler);
 
-            return new LocationWeather(location.getLatitude(),location.getLongitude(),time,mainWeather,description,tempMax,tempMin,humidity
-                    ,pressure,windSpeed,new OpenWeatherMapUnits.OpenWeatherMetric());
         } catch (IOException e) {
             Log.e(CLASS_TAG,e.getMessage());
         } catch (JSONException e) {
@@ -108,7 +85,7 @@ public class OpenWeatherMapService implements WeatherService {
         int humidity = assembler.getHumidity();
         int pressure = assembler.getPressure();
         double windSpeed = assembler.getWind();
-        return new CityWeather(city,country,time,mainWeather,description,tempMax,tempMin,humidity
+        return new OpenWeather(city,country,time,mainWeather,description,tempMax,tempMin,humidity
                 ,pressure,windSpeed, new OpenWeatherMapUnits.OpenWeatherMetric());
     }
 
