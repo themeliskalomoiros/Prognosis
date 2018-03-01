fc6f5797c9be2530f375421e4176519621c16720
Tomas Michael <tomasmichael995@gmail.com>
2018-03-01 02:59:05 +0100

    Creation of WeatherServiceImageProvider.java

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/WeatherServiceImageProvider.java
@@ -0,0 +1,9 @@
+package gr.kalymnos.sk3m3l10.prognosis.model_mvc;
+
+/**
+ * This class provides with drawables a weather service.
+ */
+
+public interface WeatherServiceImageProvider {
+    public int getImage(Object... params);
+}
