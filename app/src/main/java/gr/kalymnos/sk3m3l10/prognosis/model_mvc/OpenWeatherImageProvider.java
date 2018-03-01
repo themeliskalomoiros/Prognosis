e15cadd4bac6b33ef07677f5516f1e421cc940c5
Tomas Michael <tomasmichael995@gmail.com>
2018-03-01 03:29:08 +0100

    OpenWeatherImageProvider is also a Singleton.

--- app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherImageProvider.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/model_mvc/OpenWeatherImageProvider.java
@@ -5,6 +5,18 @@ package gr.kalymnos.sk3m3l10.prognosis.model_mvc;
  */
 
 public class OpenWeatherImageProvider implements WeatherServiceImageProvider {
+
+    public static OpenWeatherImageProvider instance = null;
+
+    private OpenWeatherImageProvider(){}
+
+    public static OpenWeatherImageProvider getInstance(){
+        if (instance==null){
+            instance=new OpenWeatherImageProvider();
+        }
+        return instance;
+    }
+
     @Override
     public int getImage(Object... params) {
         return 0;
