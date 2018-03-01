b0aaf4316db9d4d37946c80f9778e87bf115ea78
Tomas Michael <tomasmichael995@gmail.com>
2018-03-01 10:20:29 +0100

    Created an implementation of ErrorWeatherViewMvc.

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/prognosis/screens/all_forecasts/ErrorWeatherViewMvcImpl.java
@@ -0,0 +1,23 @@
+package gr.kalymnos.sk3m3l10.prognosis.screens.all_forecasts;
+
+import android.os.Bundle;
+import android.view.View;
+
+import gr.kalymnos.sk3m3l10.prognosis.view_mvc.ErrorViewMvc;
+
+public class ErrorWeatherViewMvcImpl implements ErrorViewMvc {
+    @Override
+    public View getRootView() {
+        return null;
+    }
+
+    @Override
+    public Bundle getViewState() {
+        return null;
+    }
+
+    @Override
+    public void setListener(ReloadListener listener) {
+
+    }
+}
